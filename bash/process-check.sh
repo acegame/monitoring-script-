e41f061fe080546c864ce74adbc2ab612c6e9f41
Hung-Ju, Baek <acegame@gmail.com>
2016-03-04 18:02:47 +0900

    Update process-check.sh
    alivechk 부분 수정.

--- bash/process-check.sh
+++ bash/process-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# ./consumer-check.sh $DAEMON_NAME 
-# running : 1, not running : 0 return.                                                                                              
-# service 명으로 daemon 프로세서인지 체크하고 없으면 구동중인 프로세서 리스트에서 없는지 한번더 체크 함.                                                                  
+# ./consumer-check.sh $DAEMON_NAME                                                                                                  
+# running : 1, not running : 0 return.
+# service 명으로 daemon 프로세서인지 체크하고 없으면 구동중인 프로세서 리스트에서 없는지 한번더 체크 함.
 
 if [ -z $1 ]; then 
     echo "proceess not found."
@@ -19,9 +19,10 @@ if [ -z $PROCPID ]; then
         echo "0 ($PROCESS) Process daemon not found."
         exit 1;
     fi   
+    ALIVECHK=`cat /proc/$PROCPID/status | grep Name | grep -c "$PROCESS"`
 fi
 
-ALIVECHK=`cat /proc/$PROCPID/status | grep Name | grep -c "$PROCESS"`
+ALIVECHK=`service $PROCESS status | grep -c $PROCPID`
 
 echo "$ALIVECHK"
 exit
